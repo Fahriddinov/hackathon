9a9c7c31b81d7ed8d93b8a4962b56d8de1969353
Muhammadamin <fahriddinovmuhammadamin34@gmail.com>
2024-08-06 15:01:49 +0500

    More things

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+document.getElementById('fileInput').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const previewContainer = document.getElementById('previewContainer');
+    previewContainer.innerHTML = '';
+
+    if (file) {
+        const url = URL.createObjectURL(file);
+        const preview = document.createElement(file.type.startsWith('video/') ? 'video' : 'img');
+        
+        preview.classList.add('preview');
+        preview.src = url;
+        
+        if (file.type.startsWith('video/')) {
+            preview.controls = true;
+        }
+        
+        previewContainer.appendChild(preview);
+    }
+});
+
+
 function initMap() {
     const map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 41.286887940960014, lng: 69.24195833282576 },
